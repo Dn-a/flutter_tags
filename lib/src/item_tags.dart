19b48fb8489d47429e9c94a2c3771fd50a79c79d
Oleksandr Lyshchuk <oleksandr.lyshchuk@lemberg.co.ua>
2021-07-06 12:13:21 +0300

    Fixed dataList

--- lib/src/item_tags.dart
+++ lib/src/item_tags.dart
@@ -148,9 +148,9 @@ class _ItemTagsState extends State<ItemTags> {
     // Get List<DataList> from Tags widget
     _dataListInherited = DataListInherited.of(context);
 
-    // set List length
-    if (_dataListInherited!.list!.length < _dataListInherited!.itemCount!)
-      _dataListInherited!.list!.length = _dataListInherited!.itemCount!;
+    // // set List length
+    // if (_dataListInherited!.list!.length < _dataListInherited!.itemCount!)
+    //   _dataListInherited!.list!.length = _dataListInherited!.itemCount!;
 
     if (_dataListInherited!.list!.length > (widget.index + 1) &&
         _dataListInherited!.list!.elementAt(widget.index) != null &&
