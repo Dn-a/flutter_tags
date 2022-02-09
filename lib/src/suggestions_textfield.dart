0a6b06619b62ef4d07869c1b78ef4ba8a4ebd39e
coolbootscoder <umpenn46@gmail.com>
2022-02-09 16:08:55 -0500

    Added features

--- lib/src/suggestions_textfield.dart
+++ lib/src/suggestions_textfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // InputSuggestions version 0.0.1
 // currently yield inline suggestions
@@ -26,19 +27,35 @@ class SuggestionsTextField extends StatefulWidget {
 
 class _SuggestionsTextFieldState extends State<SuggestionsTextField> {
   final _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   List<String> _matches = [];
   String? _helperText;
   bool _helperCheck = true;
+  bool _hasFocus = false;
 
   List<String>? _suggestions;
   late bool _constraintSuggestion;
   double? _fontSize;
   InputDecoration? _inputDecoration;
 
+  void _handleFocusChange() {
+    //print('flutter_tags: _handleFocusChange _hasFocus=$_hasFocus _focusNode.hasFocus=${_focusNode.hasFocus}');
+    setState(() {
+      _hasFocus = _focusNode.hasFocus;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void deactivate() {
+    _focusNode.removeListener(_handleFocusChange);
+    super.deactivate();
   }
 
   @override
@@ -75,22 +92,31 @@ class _SuggestionsTextFieldState extends State<SuggestionsTextField> {
             ),
           ),
         ),
-        TextField(
-          controller: _controller,
-          enabled: widget.tagsTextField.enabled,
-          autofocus: widget.tagsTextField.autofocus ?? true,
-          keyboardType: widget.tagsTextField.keyboardType ?? null,
-          textCapitalization: widget.tagsTextField.textCapitalization ??
-              TextCapitalization.none,
-          maxLength: widget.tagsTextField.maxLength ?? null,
-          maxLines: 1,
-          autocorrect: widget.tagsTextField.autocorrect ?? false,
-          style: widget.tagsTextField.textStyle.copyWith(
-              height: widget.tagsTextField.textStyle.height == null ? 1 : null),
-          decoration: _initialInputDecoration,
-          onChanged: (str) => _checkOnChanged(str),
-          onSubmitted: (str) => _onSubmitted(str),
-        )
+        Actions(
+          actions: {TabCompleteIntent: TabCompleteAction(textComplete: _matches.isNotEmpty ? (_matches.first) : "")},
+          child: Shortcuts(
+            shortcuts: {
+              LogicalKeySet(LogicalKeyboardKey.tab): TabCompleteIntent(_controller, _focusNode)
+            },
+            child: TextField(
+              controller: _controller,
+              focusNode: _focusNode,
+              enabled: widget.tagsTextField.enabled,
+              autofocus: widget.tagsTextField.autofocus ?? true,
+              keyboardType: widget.tagsTextField.keyboardType ?? null,
+              textCapitalization: widget.tagsTextField.textCapitalization ??
+                  TextCapitalization.none,
+              maxLength: widget.tagsTextField.maxLength ?? null,
+              maxLines: 1,
+              autocorrect: widget.tagsTextField.autocorrect ?? false,
+              style: widget.tagsTextField.textStyle.copyWith(
+                  height: widget.tagsTextField.textStyle.height == null ? 1 : null),
+              decoration: _initialInputDecoration,
+              onChanged: (str) => _checkOnChanged(str),
+              onSubmitted: (str) => _onSubmitted(str),
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -127,14 +153,20 @@ class _SuggestionsTextFieldState extends State<SuggestionsTextField> {
   ///OnSubmitted
   void _onSubmitted(String str) {
     var onSubmitted = widget.onSubmitted;
-
+    String original = str;
     if (_suggestions != null && _matches.isNotEmpty) str = _matches.first;
 
     if (widget.tagsTextField.lowerCase) str = str.toLowerCase();
 
     str = str.trim();
 
-    if (_suggestions != null) {
+    if (!widget.tagsTextField.pickSuggestionOnEnter && original != str) {
+      if (onSubmitted != null) onSubmitted(original);
+      setState(() {
+        _matches = [];
+      });
+      _controller.clear();
+    } else if (_suggestions != null) {
       if (_matches.isNotEmpty || !_constraintSuggestion) {
         if (onSubmitted != null) onSubmitted(str);
         setState(() {
@@ -145,6 +177,12 @@ class _SuggestionsTextFieldState extends State<SuggestionsTextField> {
     } else if (str.isNotEmpty) {
       if (onSubmitted != null) onSubmitted(str);
       _controller.clear();
+    }
+    if (widget.tagsTextField.keepFocus && _hasFocus) {
+      //print('flutter_tags: _onSubmitted keepFocus=${widget.tagsTextField.keepFocus}, _hasFocus=$_hasFocus');
+      Future.delayed(Duration(milliseconds: 100), () {
+        _focusNode.requestFocus();
+      });
     }
   }
 
@@ -183,8 +221,10 @@ class TagsTextField {
       this.duplicates = false,
       this.suggestions,
       this.constraintSuggestion = true,
+      this.pickSuggestionOnEnter = true,
       this.autocorrect,
       this.autofocus,
+      this.keepFocus = false,
       this.hintText,
       this.hintTextColor,
       this.suggestionTextColor,
@@ -209,7 +249,9 @@ class TagsTextField {
   /// Allows you to insert tags not present in the list of suggestions
   final bool constraintSuggestion;
   final bool lowerCase;
+  final bool pickSuggestionOnEnter;
   final bool? autofocus;
+  final bool keepFocus;
   final String? hintText;
   final Color? hintTextColor;
   final Color? suggestionTextColor;
@@ -220,4 +262,27 @@ class TagsTextField {
   final int? maxLength;
   final OnSubmittedCallback? onSubmitted;
   final OnChangedCallback? onChanged;
+}
+
+class TabCompleteIntent extends Intent {
+  const TabCompleteIntent(this.textController, this.focusNode);
+  final TextEditingController textController;
+  final FocusNode focusNode;
+}
+
+class TabCompleteAction extends Action {
+
+  final String textComplete;
+
+  TabCompleteAction({required this.textComplete});
+
+  @override
+  Object invoke(covariant Intent intent) {
+    if (intent is TabCompleteIntent && textComplete != '') {
+      intent.textController.value = TextEditingValue(text: textComplete);
+    } else if (intent is TabCompleteIntent) {
+      intent.focusNode.nextFocus();
+    }
+    return '';
+  }
 }
